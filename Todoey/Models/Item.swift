dd21b78f646392cf88c936fe9c871b054e25a481
buchnersantos <pbuchner.santos@gmail.com>
2018-11-09 11:22:37 +0100

    Implementation of Items Object complete

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Patricia Buchner on 11/9/18.
+//  Copyright © 2018 Patricia Buchner. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
--- Todoey/New Group1/TodoListViewController.swift
+++ Todoey/New Group1/TodoListViewController.swift
@@ -10,59 +10,62 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Bananas","Apples","Pears"]
+    var itemArray = [Item]()
     
     var defaults = UserDefaults.standard
+    
+    let defaultsKey = "TodoListArray"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as! [String]? {
-            itemArray = items
-        }
+        let item1 = Item()
+        item1.title = "Apples"
+        itemArray.append(item1)
+        
+        let item2 = Item()
+        item2.title = "Bananas"
+        itemArray.append(item2)
+        
+        let item3 = Item()
+        item3.title = "Pijamas"
+        itemArray.append(item3)
+        
+        //if let items = defaults.array(forKey: defaultsKey) as? [Item] {
+        //    itemArray = items
+        //}
     }
     
-    //
-    //MARK -- Table view datasource methods
-    //       what cells should display and how many rows
+    
+    // MARK - TableView datasource methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //Resuable cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.accessoryType = item.done == true ? .checkmark : .none
+
+        cell.textLabel?.text = item.title
         
         return cell
     }
     
-    
-    //
     // MARK - TableView delegate methods
-    //
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(indexPath.row) // print number when clicked
-        //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
-    //
     //  MARK - Add New Items
-    //  UIAlert with textfield when this button is clicked and append to end of array
-    //
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -70,24 +73,22 @@ class TodoListViewController: UITableViewController {
 
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+          
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            //self.defaults.set(self.itemArray, forKey: self.defaultsKey)
+            
+            self.tableView.reloadData()
+            
+        }
+        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
-        
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //what happens once user clicks submit
-            print(textField.text)
-          
-            self.itemArray.append(textField.text!)
-            
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
-            
-            
-        }
-        
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
